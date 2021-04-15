259eb78a2489f000327b98f5431ac7644aa32ccb
Ws1118 <2865094521@qq.com>
2021-04-15 16:00:16 +0800

    bili

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  created() {
+    this.axios.get("http://localhost:3000/popular").then(res => {
+      console.log(res.data);
+    });
+  }
+};
+</script>
+
+<style>
+</style>
